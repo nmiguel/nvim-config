0bb00696c9a3bbc3a7e72d8851439477ec21b9c0
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-04-18 22:44:33 +0100

    stop auto commenting

--- lua/nomig/autocmd.lua
+++ lua/nomig/autocmd.lua
@@ -1,15 +1,22 @@
-
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-    desc = 'Clear whitespaces on end of line when saving',
-    group = vim.api.nvim_create_augroup('nomig-clear-whitespace', { clear = true}),
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	desc = "Clear whitespaces on end of line when saving",
+	group = vim.api.nvim_create_augroup("nomig-clear-whitespace", { clear = true }),
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Stop auto commenting",
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "r", "o" })
+	end,
 })
