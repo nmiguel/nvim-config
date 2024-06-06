4f8d4f8eab88365c2d1427278549a164809ef6cf
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-06-06 23:42:39 +0100

    trouble on the right

--- lua/nomig/plugins/trouble.lua
+++ lua/nomig/plugins/trouble.lua
@@ -1,13 +1,18 @@
 return {
     "folke/trouble.nvim",
-    branch = "dev", -- IMPORTANT!
+    -- branch = "dev", -- IMPORTANT!
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         focus = true,
+        win = {
+            type = "split",
+            position = "right",
+            size = 60,
+        },
     },
     keys = {
-        { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Document Diagnostics (Trouble)" },
-        { "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Workspace Diagnostics (Trouble)" },
+        { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace Diagnostics (Trouble)" },
+        { "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Document Diagnostics (Trouble)" },
         { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
         -- {
         --     "[d",
