6ae0d77546c2f4fa05c5f8d45f9b141a82b71b9e
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-06-25 13:44:30 +0100

    visual block mode

--- lua/nomig/remap.lua
+++ lua/nomig/remap.lua
@@ -10,8 +10,6 @@ vim.keymap.set = function(mode, lhs, rhs, opts)
     return keymap_set(mode, lhs, rhs, opts)
 end
 
-
-
 vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>oe','<cmd>silent !explorer.exe .<cr>' , {noremap = true})
 
@@ -20,7 +18,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
+
+-- vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
@@ -33,10 +32,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<c-v>")
 vim.keymap.set("n", "<leader>p", "<nop>")  -- I press this so much before deciding on telescope
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz")
-
