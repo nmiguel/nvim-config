a167f36309b5b071dcb7ead2cd144d342af38986
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-06-25 13:44:55 +0100

    global functions

--- lua/nomig/init.lua
+++ lua/nomig/init.lua
@@ -2,3 +2,19 @@ require("nomig.remap")
 require("nomig.set")
 require("nomig.lazy")
 require("nomig.autocmd")
+
+
+-- Better reloading and printing
+P = function(v)
+    print(vim.inspect(v))
+    return v
+end
+
+RELOAD = function(...)
+    return require("plenary.reload").reload_module(...)
+end
+
+R = function(name)
+    RELOAD(name)
+    return require(name)
+end
