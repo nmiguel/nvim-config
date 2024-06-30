875dd83e3db34d9121adb87a57d63c7e01ca7b64
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-06-30 19:34:17 +0100

    yank assassin, netrw icons

--- lua/nomig/plugins/init.lua
+++ lua/nomig/plugins/init.lua
@@ -17,4 +17,26 @@ return {
 		event = { "VeryLazy" },
 		dependencies = { "kana/vim-textobj-user" },
 	},
+	{
+		"svban/YankAssassin.nvim",
+		config = function()
+			require("YankAssassin").setup({
+				auto = true,
+			})
+		end,
+	},
+	{
+		"prichrd/netrw.nvim",
+		opts = {
+			-- Put your configuration here, or leave the object empty to take the default
+			-- configuration.
+			icons = {
+				symlink = "", -- Symlink icon (directory and file)
+				directory = "", -- Directory icon
+				file = "", -- File icon
+			},
+			use_devicons = true, -- Uses nvim-web-devicons if true, otherwise use the file icon specified above
+			mappings = {}, -- Custom key mappings}
+		},
+	},
 }
