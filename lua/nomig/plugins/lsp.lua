91e7024a9fac19c00c78295ba4918c757b2b0292
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-07-27 21:21:04 +0100

    changed structure of lsp (c# broken)

--- lua/nomig/plugins/lsp.lua
+++ lua/nomig/plugins/lsp.lua
@@ -1,12 +1,6 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
-		-- ft = {
-		--     "python",
-		--     "lua",
-		--     "cs",
-		--     "yaml"
-		-- },
 
 		event = "BufEnter",
 		dependencies = {
@@ -16,6 +10,9 @@ return {
 			-- Autocompletion
 			"L3MON4D3/LuaSnip",
 			"nvim-telescope/telescope.nvim",
+			{ "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+
+			-- Lua extra config
 			{
 				{
 					"folke/lazydev.nvim",
@@ -39,7 +36,6 @@ return {
 						})
 					end,
 				},
-				-- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 			},
 		},
 
@@ -51,8 +47,11 @@ return {
 
 			vim.diagnostic.config({
 				virtual_text = {
+					spacing = 4,
 					prefix = "",
+					source = "if_many",
 				},
+				update_in_insert = false,
 			})
 
 			-- vim.lsp.inlay_hint.enable()
@@ -84,138 +83,122 @@ return {
 				border = _border,
 			})
 
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
+			require("mason").setup({
 				ensure_installed = { "pyright", "gopls", "yamlls", "jsonls", "zls" },
-				handlers = {
-					function(server_name) -- default handler (optional)
-						require("lspconfig")[server_name].setup({})
-					end,
-					["lua_ls"] = function()
-						require("lspconfig").lua_ls.setup({
-							on_init = function(client)
-								local path = client.workspace_folders[1].name
-								if
-									vim.loop.fs_stat(path .. "/.luarc.json")
-									or vim.loop.fs_stat(path .. "/.luarc.jsonc")
-								then
-									return
-								end
+			})
+			require("mason-lspconfig").setup({})
 
-								client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-									runtime = {
-										-- Tell the language server which version of Lua you're using
-										-- (most likely LuaJIT in the case of Neovim)
-										version = "LuaJIT",
-									},
-									-- Make the server aware of Neovim runtime files
-									workspace = {
-										checkThirdParty = false,
-										library = {
-											vim.env.VIMRUNTIME,
-											-- Depending on the usage, you might want to add additional paths here.
-											-- "${3rd}/luv/library"
-											-- "${3rd}/busted/library",
-										},
-										-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-										-- library = vim.api.nvim_get_runtime_file("", true)
-									},
-								})
-							end,
-							settings = {
-								Lua = {
-									runtime = { version = "LuaJIT" },
-									diagnostics = {
-										globals = { "vim", "require" },
-									},
-									workspace = {
-										-- Make the server aware of Neovim runtime files
-										library = vim.api.nvim_get_runtime_file("", true),
-									},
-									-- Do not send telemetry data containing a randomized but unique identifier
-									telemetry = {
-										enable = false,
-									},
-								},
-							},
-						})
-					end,
-					["pyright"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.pyright.setup({
-							settings = {
-								python = {
-									analysis = {
-										autoSearchPaths = true,
-										diagnosticMode = "openFilesOnly",
-										useLibraryCodeForTypes = true,
-										typeCheckingMode = "off",
-										exclude = { "venv" },
-									},
-								},
-							},
-						})
-					end,
-					["gopls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.gopls.setup({
-							settings = {
-								gopls = {
-									gofumpt = true,
-									codelenses = {
-										gc_details = false,
-										generate = true,
-										regenerate_cgo = true,
-										run_govulncheck = true,
-										test = true,
-										tidy = true,
-										upgrade_dependency = true,
-										vendor = true,
-									},
-									hints = {
-										assignVariableTypes = true,
-										compositeLiteralFields = true,
-										compositeLiteralTypes = true,
-										constantValues = true,
-										functionTypeParameters = true,
-										parameterNames = true,
-										rangeVariableTypes = true,
-									},
-									analyses = {
-										fieldalignment = true,
-										nilness = true,
-										unusedparams = true,
-										unusedwrite = true,
-										useany = true,
-									},
-									usePlaceholders = true,
-									completeUnimported = true,
-									staticcheck = true,
-									directoryFilters = {
-										"-.git",
-										"-.vscode",
-										"-.idea",
-										"-.vscode-test",
-										"-node_modules",
-									},
-									semanticTokens = true,
-								},
-							},
-						})
-					end,
-					["jsonls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.jsonls.setup({
-							settings = {},
-						})
-					end,
-					["yamlls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.yamlls.setup({
-							settings = {},
-						})
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						workspace = {
+							checkThirdParty = false,
+						},
+						codeLens = {
+							enable = true,
+						},
+						completion = {
+							callSnippet = "Replace",
+						},
+						doc = {
+							privateName = { "^_" },
+						},
+						hint = {
+							enable = true,
+							setType = false,
+							paramType = true,
+							paramName = "Disable",
+							semicolon = "Disable",
+							arrayIndex = "Disable",
+						},
+					},
+				},
+			})
+			lspconfig.pyright.setup({
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							diagnosticMode = "openFilesOnly",
+							useLibraryCodeForTypes = true,
+							typeCheckingMode = "off",
+							exclude = { "venv" },
+						},
+					},
+				},
+			})
+			lspconfig.gopls.setup({
+				settings = {
+					gopls = {
+						gofumpt = true,
+						codelenses = {
+							gc_details = false,
+							generate = true,
+							regenerate_cgo = true,
+							run_govulncheck = true,
+							test = true,
+							tidy = true,
+							upgrade_dependency = true,
+							vendor = true,
+						},
+						hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							functionTypeParameters = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						},
+						analyses = {
+							fieldalignment = true,
+							nilness = true,
+							unusedparams = true,
+							unusedwrite = true,
+							useany = true,
+						},
+						usePlaceholders = true,
+						completeUnimported = true,
+						staticcheck = true,
+						directoryFilters = {
+							"-.git",
+							"-.vscode",
+							"-.idea",
+							"-.vscode-test",
+							"-node_modules",
+						},
+						semanticTokens = true,
+					},
+				},
+			})
+			lspconfig.jsonls.setup({
+				settings = {},
+			})
+			lspconfig.yamlls.setup({
+				settings = {},
+			})
+			lspconfig.omnisharp.setup({
+				handlers = {
+					["textDocument/definition"] = function(...)
+						return require("omnisharp_extended").handler(...)
 					end,
 				},
+				keys = {
+					{
+						"gd",
+						function()
+							require("omnisharp_extended").telescope_lsp_definitions()
+						end,
+						desc = "Goto Definition",
+					},
+				},
+				enable_roslyn_analyzers = true,
+				organize_imports_on_format = true,
+				enable_import_completion = true,
+                root_dir = function ()
+                    return vim.loop.cwd() -- current working directory
+                end,
 			})
 		end,
 	},
