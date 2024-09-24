6f3abf1b7502abd1c086850eec3193326c3334d9
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-09-25 00:22:24 +0100

    telescope frecency

--- lua/nomig/plugins/telescope.lua
+++ lua/nomig/plugins/telescope.lua
@@ -2,10 +2,34 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{
+			-- Uses frecency and other metrics to sort files
+			"danielfalk/smart-open.nvim",
+			branch = "0.2.x",
+			config = function()
+				require("telescope").load_extension("smart_open")
+			end,
+			keys = {
+				{
+					mode = "n",
+					"<leader>pf",
+					function()
+						require("telescope").extensions.smart_open.smart_open({
+							cwd_only = true,
+						})
+					end,
+				},
+			},
+			dependencies = {
+				"kkharji/sqlite.lua",
+				-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+				{ "nvim-telescope/telescope-fzy-native.nvim" },
+			},
+		},
 	},
 	lazy = false,
 	keys = {
-		{ mode = "n", "<leader>pf", require("telescope.builtin").find_files },
+		-- { mode = "n", "<leader>pf", require("telescope.builtin").find_files },
 		{
 			mode = "n",
 			"<leader>paf",
@@ -25,7 +49,15 @@ return {
 						["<esc>"] = actions.close,
 					},
 				},
-                path_display ={  "filename_first" },
+				path_display = { "filename_first" },
+			},
+			extensions = {
+				smart_open = {
+                    open_buffer_indicators = {
+                        previous = ">",
+                        others = "-"
+                    }
+				},
 			},
 		})
 	end,
