dd5a8fc082067804c2c3c0f1b46f11c37b807673
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-12-01 22:44:26 +0000

    neoscroll

--- lua/nomig/plugins/init.lua
+++ lua/nomig/plugins/init.lua
@@ -1,4 +1,48 @@
 return {
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup({
+				easing = "cubic",
+			})
+
+			local duration = 50
+			local neoscroll = require("neoscroll")
+			local keymap = {
+				["<C-u>"] = function()
+					neoscroll.ctrl_u({ duration = duration })
+				end,
+				["<C-d>"] = function()
+					neoscroll.ctrl_d({ duration = duration })
+				end,
+				["<C-b>"] = function()
+					neoscroll.ctrl_b({ duration = duration * 2 })
+				end,
+				["<C-f>"] = function()
+					neoscroll.ctrl_f({ duration = duration * 2 })
+				end,
+				["<C-y>"] = function()
+					neoscroll.scroll(-0.1, { move_cursor = false, duration = duration / 2 })
+				end,
+				["<C-e>"] = function()
+					neoscroll.scroll(0.1, { move_cursor = false, duration = duration / 2 })
+				end,
+				["zt"] = function()
+					neoscroll.zt({ half_win_duration = duration })
+				end,
+				["zz"] = function()
+					neoscroll.zz({ half_win_duration = duration })
+				end,
+				["zb"] = function()
+					neoscroll.zb({ half_win_duration = duration })
+				end,
+			}
+			local modes = { "n", "v", "x" }
+			for key, func in pairs(keymap) do
+				vim.keymap.set(modes, key, func)
+			end
+		end,
+	},
 	"stevearc/dressing.nvim",
 	"tpope/vim-vinegar",
 	{
