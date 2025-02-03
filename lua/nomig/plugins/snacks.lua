e0ad231dcbc7a4a2d4cb259f3db8a55fe394926e
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2025-02-03 18:19:23 +0000

    snacks replace indent, telescope

--- lua/nomig/plugins/snacks.lua
+++ lua/nomig/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
 	priority = 1000,
 	lazy = false,
 	config = function()
+		local hl = "WinSeparator"
 		require("snacks").setup({
 			styles = {},
 			scroll = {
@@ -12,15 +13,104 @@ return {
 			words = { enabled = true },
 			notifier = { enabled = false },
 			quickfile = { enabled = false },
-
 			bigfile = { enabled = true },
 			statuscolumn = { enabled = true },
+
+            scope = {
+                cursor = false,
+                siblings = true,
+            },
+
+			indent = {
+				only_current = true,
+				enabled = true,
+				animate = { enabled = false },
+				scope = { enabled = false, hl = hl },
+				chunk = {
+					enabled = true,
+					only_current = true,
+					hl = hl,
+					char = {
+						corner_top = "╭",
+						corner_bottom = "╰",
+						arrow = "",
+					},
+				},
+			},
+
+			picker = {
+				layout = "telescope",
+				matcher = {
+					fuzzy = true,
+					smartcase = true,
+					frecency = true,
+					sort_empty = true,
+				},
+				formatters = {
+					file = {
+						filename_first = true,
+					},
+				},
+				win = {
+					input = {
+						keys = {
+							["<Esc>"] = { "close", mode = { "n", "i" } },
+						},
+					},
+				},
+			},
 		})
 		vim.api.nvim_create_user_command("Github", function()
 			Snacks.gitbrowse()
 		end, {})
 	end,
 	keys = {
+		-- Pickers
+		{
+			mode = "n",
+			"<leader>PP",
+			function()
+				Snacks.picker()
+			end,
+		},
+		{
+			mode = "n",
+			"<leader>pf",
+			function()
+				Snacks.picker.recent({
+					filter = { cwd = true }, -- replicate `cwd_only = true`
+				})
+			end,
+		},
+		{
+			mode = "n",
+			"<leader>ps",
+			function()
+				Snacks.picker.grep()
+			end,
+		},
+		{
+			mode = "n",
+			"<leader>ds",
+			function()
+				Snacks.picker.help()
+			end,
+		},
+		{
+			mode = "n",
+			"<leader>:",
+			function()
+				Snacks.picker.commands()
+			end,
+		},
+		{
+			mode = { "v", "n" },
+			"<leader>*",
+			function()
+				Snacks.picker.grep_word()
+			end,
+		},
+		-- Other functions
 		{
 			"<leader>gf",
 			function()
