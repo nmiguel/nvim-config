4701d5f79524d484859441faa08a7caa91bf00a1
Nuno Ramos <nmiguel123@gmail.com>
2025-06-13 19:09:59 +0100

    supermaven

--- lua/nomig/plugins/copilot.lua
+++ lua/nomig/plugins/copilot.lua
@@ -1,67 +1,21 @@
 return {
-	"zbirenbaum/copilot.lua",
-	keys = {
-		{
-			"<leader>ce",
-			function()
-                vim.cmd("Copilot enable")
-                print("Copilot enabled")
-            end,
-			mode = "n",
-			desc = "Enable Copilot",
-		},
-		{
-			"<leader>cd",
-			function()
-                vim.cmd("Copilot disable")
-                print("Copilot disabled")
-            end,
-			mode = "n",
-			desc = "Disable Copilot",
-		},
-	},
+	"supermaven-inc/supermaven-nvim",
 	config = function()
-		require("copilot").setup({
-			panel = {
-				enabled = false,
-				-- auto_refresh = false,
-				-- keymap = {
-				-- 	jump_prev = "[[",
-				-- 	jump_next = "]]",
-				-- 	accept = "<C-O>",
-				-- 	refresh = "gr",
-				-- 	open = "<M-CR>",
-				-- },
-				-- layout = {
-				-- 	position = "bottom", -- | top | left | right
-				-- 	ratio = 0.4,
-				-- },
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<C-j>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C->>",
 			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<C-J>",
-					accept_word = false,
-					accept_line = false,
-					-- next = "<C-L>",
-					-- prev = "<C-H>",
-					dismiss = "<C-]>",
-				},
-			},
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-			server_opts_overrides = {},
 		})
+
+		vim.keymap.set("n", "<leader>cd", function()
+			vim.cmd("SupermavenStop")
+			vim.notify("Supermaven stopped")
+		end, { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>ce", function()
+			vim.cmd("SupermavenStart")
+			vim.notify("Supermaven started")
+		end, { noremap = true, silent = true })
 	end,
 }
