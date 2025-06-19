c6b7f89853172768f7531e6aa526e4805cbb198e
Nuno Ramos <nmiguel123@gmail.com>
2025-06-19 22:35:30 +0100

    ts and js support

--- lua/nomig/plugins/lsp_servers/init.lua
+++ lua/nomig/plugins/lsp_servers/init.lua
@@ -3,5 +3,6 @@ require("nomig.plugins.lsp_servers.python")
 require("nomig.plugins.lsp_servers.json")
 require("nomig.plugins.lsp_servers.yaml")
 require("nomig.plugins.lsp_servers.go")
+require("nomig.plugins.lsp_servers.ts")
 
 return {}
