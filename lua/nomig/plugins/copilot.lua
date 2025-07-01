ed1c08d39c275cf44fbd4a546ac49c5190523d66
Nuno Ramos <nmiguel123@gmail.com>
2025-07-02 00:12:31 +0100

    supermaven functions

--- lua/nomig/plugins/copilot.lua
+++ lua/nomig/plugins/copilot.lua
@@ -1,5 +1,25 @@
 return {
 	"supermaven-inc/supermaven-nvim",
+	keys = {
+		{
+			mode = "n",
+			"<leader>cd",
+			function()
+				vim.cmd("SupermavenStop")
+				vim.notify("Supermaven stopped")
+			end,
+			desc = "Stop Supermaven",
+		},
+		{
+			mode = "n",
+			"<leader>ce",
+			function()
+				vim.cmd("SupermavenStart")
+				vim.notify("Supermaven started")
+			end,
+			desc = "Start Supermaven",
+		},
+	},
 	config = function()
 		require("supermaven-nvim").setup({
 			keymaps = {
@@ -8,14 +28,5 @@ return {
 				accept_word = "<C->>",
 			},
 		})
-
-		vim.keymap.set("n", "<leader>cd", function()
-			vim.cmd("SupermavenStop")
-			vim.notify("Supermaven stopped")
-		end, { noremap = true, silent = true })
-		vim.keymap.set("n", "<leader>ce", function()
-			vim.cmd("SupermavenStart")
-			vim.notify("Supermaven started")
-		end, { noremap = true, silent = true })
 	end,
 }
