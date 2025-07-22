d0ac203d43fd67458923eaf24d3261b666556919
Nuno Ramos <nuno.ramos.ext@triviumpackaging.com>
2025-07-22 12:46:30 +0200

    indent keeps visual mode

--- lua/nomig/remap.lua
+++ lua/nomig/remap.lua
@@ -1,21 +1,11 @@
 vim.g.mapleader = " "
 
--- make all keymaps silent by default
-local keymap_set = vim.keymap.set
----@diagnostic disable-next-line: duplicate-set-field
-vim.keymap.set = function(mode, lhs, rhs, opts)
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    return keymap_set(mode, lhs, rhs, opts)
-end
-
-vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>oe','<cmd>silent !explorer.exe .<cr>' , {noremap = true})
+vim.keymap.set("i", "<C-H>", "<C-W>", { noremap = true })
+vim.keymap.set("n", "<leader>oe", "<cmd>silent !explorer.exe .<cr>", { noremap = true })
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
--- vim.keymap.set("n", "J", "mzJ`z$")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
@@ -24,18 +14,21 @@ vim.keymap.set("n", "G", "Gzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v"} , "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set( {"n", "v" } , "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<c-v>")
-vim.keymap.set("n", "<leader>p", "<nop>")  -- I press this so much before deciding on telescope
+vim.keymap.set("n", "<leader>p", "<nop>") -- I press this so much before deciding on telescope
 
 vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("v", "gk", ":norm gcc<CR>")
 
-vim.api.nvim_set_keymap('n', 'j','gj' , {noremap = true})
-vim.api.nvim_set_keymap('n', 'k','gk' , {noremap = true})
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
