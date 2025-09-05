81011a65710582739327d8be51e3fa059b82ffcb
Nuno Ramos <nuno.ramos.ext@triviumpackaging.com>
2025-09-05 10:11:20 +0200

    mimic smart splits

--- lua/nomig/remap.lua
+++ lua/nomig/remap.lua
@@ -41,4 +41,11 @@ vim.api.nvim_set_keymap('n', 'j', 'gj', {noremap = true})
 vim.api.nvim_set_keymap('n', 'k', 'gk', {noremap = true})
 vim.api.nvim_set_keymap('n', 'gp', '`[v`]', {noremap = true})
 
+
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', {noremap = true})
+
+
 -- vim.api.nvim_set_keymap('x', '<leader>f', ':fold<cr>', {noremap = true})
