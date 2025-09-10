3388b7b8785bd75faaeef033fc28c5afa9d2c7ca
Nuno Ramos <nmiguel123@gmail.com>
2025-09-10 23:30:57 +0100

    persistence as session manager

--- lua/nomig/plugins/sessions.lua
+++ lua/nomig/plugins/sessions.lua
@@ -1,38 +1,28 @@
 return {
-    "Shatur/neovim-session-manager",
-    dependencies = 'nvim-lua/plenary.nvim',
-    lazy = false,
-    priority = 1000,
+	"folke/persistence.nvim",
+	dependencies = "nvim-lua/plenary.nvim",
+	lazy = false,
+	priority = 1000,
 
-    keys = {
-        {
-            "<leader>ls",
-            function() require("session_manager").load_session(true) end,
-            mode = "n",
-            desc = "Load session"
-        }
-    },
-
-    config = function()
-        require('session_manager').setup( {
-            sessions_dir = vim.fn.stdpath("data") .. "/sessions",
-            autosave_ignore_buftypes = {}, -- All buffers of these bufer types will be closed before the session is saved.
-            autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
-            autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
-            max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
-        })
-        -- Auto save session
-        vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-            callback = function ()
-                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                    -- Don't save while there's any 'nofile' buffer open.
-                    if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
-                        return
-                    end
-                end
-                require("session_manager").save_current_session()
-            end
-        })
-    end
+	config = function()
+		require("persistence").setup({
+			need = 1,
+			branch = false,
+		})
+		-- Auto save session
+		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+			callback = function()
+				require("persistence").save()
+			end,
+		})
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				if vim.fn.argc() == 0 then
+					vim.schedule(function()
+						require("persistence").load()
+					end)
+				end
+			end,
+		})
+	end,
 }
-
