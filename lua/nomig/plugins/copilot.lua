97c07198c7454734a338823dc487ea2d146c357a
Nuno Ramos <nmiguel123@gmail.com>
2025-10-31 22:08:32 +0000

    supermaven sunset

--- lua/nomig/plugins/copilot.lua
+++ lua/nomig/plugins/copilot.lua
@@ -1,35 +1,30 @@
 return {
-	"supermaven-inc/supermaven-nvim",
-	keys = {
-		{
-			mode = "n",
-			"<leader>cd",
-			function()
-				vim.cmd("SupermavenStop")
-				vim.notify("Supermaven stopped")
-			end,
-			desc = "Stop Supermaven",
-		},
-		{
-			mode = "n",
-			"<leader>ce",
-			function()
-				vim.cmd("SupermavenStart")
-				vim.notify("Supermaven started")
-			end,
-			desc = "Start Supermaven",
-		},
+	"zbirenbaum/copilot.lua",
+	dependencies = {
+		-- "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
 	},
+	cmd = "Copilot",
+	event = "InsertEnter",
+    keys = {
+        { "<leader>ce", desc = "Enable Copilot" },
+        { "<leader>cd", desc = "Disable Copilot" },
+    },
 	config = function()
-		require("supermaven-nvim").setup({
-			keymaps = {
-				accept_suggestion = "<C-j>",
-				clear_suggestion = "<C-]>",
-				accept_word = "<C->>",
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<C-j>",
+				},
 			},
 		})
-		-- vim.cmd("SupermavenStart")
-		-- vim.cmd("SupermavenStop")
+		vim.keymap.set("n", "<leader>ce", function()
+			vim.cmd("silent Copilot enable")
+            print("Copilot enabled")
+		end, { desc = "Enable Copilot" })
+		vim.keymap.set("n", "<leader>cd", function()
+            vim.cmd("silent Copilot disable")
+            print("Copilot disabled")
+		end, { desc = "Disable Copilot" })
 	end,
-	event = "InsertEnter",
 }
