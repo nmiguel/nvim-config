a323479b9d4e111180ebc3ece87c16929dfe1449
Nuno Ramos <nuno.ramos.ext@triviumpackaging.com>
2025-11-12 16:32:28 +0100

    removed lazydev integration

--- lua/nomig/plugins/cmp.lua
+++ lua/nomig/plugins/cmp.lua
@@ -56,16 +56,7 @@ return {
 			},
 			cmdline = {enabled = false},
 			sources = {
-				-- default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 				default = { "lsp", "path", "snippets", "buffer" },
-				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						-- make lazydev completions top priority (see `:h blink.cmp`)
-						score_offset = 100,
-					},
-				},
 			},
 			snippets = {
 				preset = "luasnip",
