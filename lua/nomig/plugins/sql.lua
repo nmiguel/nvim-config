c1b0ef705cd49fe955bf02d12c288a7068671ddc
Nuno Ramos <nmiguel123@gmail.com>
2026-02-04 17:33:04 +0000

    sql completions

--- lua/nomig/plugins/sql.lua
+++ lua/nomig/plugins/sql.lua
@@ -1,20 +1,37 @@
 return {
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.db_ui_winwidth = 50
-    vim.g.db_ui_auto_execute_table_helpers = 1
-    vim.g.db_ui_disable_progress_bar = 1
-  end,
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.db_ui_winwidth = 50
+			vim.g.db_ui_auto_execute_table_helpers = 1
+			vim.g.db_ui_disable_progress_bar = 1
+		end,
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				per_filetype = {
+					sql = { "snippets", "dadbod", "buffer" },
+				},
+				-- add vim-dadbod-completion to your completion providers
+				providers = {
+					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+				},
+			},
+		},
+	},
 }
