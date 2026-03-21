0f0c74c519e31033c588a2c8c5487bb7bd9a60bd
Nuno Ramos <nmiguel123@gmail.com>
2026-03-21 17:40:35 +0000

    clipboard via osc52

--- lua/nomig/set.lua
+++ lua/nomig/set.lua
@@ -60,3 +60,16 @@ vim.opt.foldtext = "v:lua.CustomFoldText()"
 -- Disable deprecation warnings
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
+
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
