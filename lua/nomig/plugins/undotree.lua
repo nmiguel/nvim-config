8207a8a4823425bba8d8e400ea827c4f46d248a4
Nuno Ramos <nmiguel123@gmail.com>
2026-04-02 00:01:15 +0100

    builtin undo

--- lua/nomig/plugins/undotree.lua
+++ lua/nomig/plugins/undotree.lua
@@ -1,7 +1,13 @@
-return {
-	"XXiaoA/atone.nvim",
-	keys = {
-		{ mode = "n", "<leader>u", "<cmd>Atone<CR>" },
-	},
-	opts = {},
-}
+vim.cmd("packadd nvim.undotree")
+
+vim.keymap.set("n", "<leader>u", ":Undotree<CR>", { desc = "Toggle undotree" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "nvim-undotree",
+	callback = function()
+		vim.cmd.wincmd("H")
+		vim.api.nvim_win_set_width(0, 45)
+	end,
+})
+
+return {}
