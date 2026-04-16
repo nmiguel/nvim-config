ffb4e23e4f549928012c08ec6f3cfc43249707ed
Nuno Ramos <nmiguel123@gmail.com>
2026-04-16 14:54:50 +0100

    better copilot integration

--- lua/nomig/plugins/copilot.lua
+++ lua/nomig/plugins/copilot.lua
@@ -5,26 +5,27 @@ return {
 	},
 	cmd = "Copilot",
 	event = "InsertEnter",
-    keys = {
-        { "<leader>ce", desc = "Enable Copilot" },
-        { "<leader>cd", desc = "Disable Copilot" },
-    },
+	keys = {
+		{ "<leader>ce", desc = "Enable Copilot" },
+		{ "<leader>cd", desc = "Disable Copilot" },
+	},
 	config = function()
 		require("copilot").setup({
 			suggestion = {
 				auto_trigger = true,
 				keymap = {
 					accept = "<C-j>",
+					accept_word = "<C-h>",
 				},
 			},
 		})
 		vim.keymap.set("n", "<leader>ce", function()
 			vim.cmd("silent Copilot enable")
-            print("Copilot enabled")
+			print("Copilot enabled")
 		end, { desc = "Enable Copilot" })
 		vim.keymap.set("n", "<leader>cd", function()
-            vim.cmd("silent Copilot disable")
-            print("Copilot disabled")
+			vim.cmd("silent Copilot disable")
+			print("Copilot disabled")
 		end, { desc = "Disable Copilot" })
 	end,
 }
